9b3e8d4ad7e24c35a3c56aaba6714567c03f1754
pc <pc@pc-PC>
2015-04-24 00:50:32 -0500

    Se crea una interfaz llamada ProductoDAO

---
+++ codigo/EjerccioJPAclases/src/main/java/edu/co/sena/tennishop/midelo/jpa/dao/interfaces/PedidoDAO.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.tennishop.midelo.jpa.dao.interfaces;
+
+import edu.co.sena.tennisshop.integracion.jpa.entitis.Pedido;
+import java.util.List;
+
+/**
+ *
+ * @author pc
+ */
+public interface PedidoDAO {
+
+    public void insert(Pedido Entity);
+
+    public void update(Pedido Entity);
+
+    public void delete(Pedido Entity);
+
+    public List<Pedido> findByAll();
+
+    public Pedido findByFacturaIdFactura();
+
+    public List<Pedido> findByTotal(Object total);
+
+    public List<Pedido> findBySubtotal(Object subtotal);
+
+    public List<Pedido> findByImpuestos(Object Impuestos);
+
+}
