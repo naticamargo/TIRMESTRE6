9d94a46e2bf32c6be1d4bc550bc8211e0d2f14e4
pc <pc@pc-PC>
2015-04-29 23:25:04 -0500

    Se crea una interfaz llamada DAOItem.

---
+++ codigo/EjerccioJPAclases/src/main/java/edu/co/sena/tennisshop/modelo/factory/DAOItem.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.tennisshop.modelo.factory;
+
+import edu.co.sena.tennishop.midelo.jpa.dao.interfaces.ItemDAO;
+
+/**
+ *
+ * @author pc
+ */
+public interface DAOItem {
+    public ItemDAO createItemDAO();
+}
