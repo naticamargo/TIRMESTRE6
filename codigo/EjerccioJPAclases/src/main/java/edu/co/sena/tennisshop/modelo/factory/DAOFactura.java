301ebbf5f6e65567754e7ed38da74acfd4199c75
pc <pc@pc-PC>
2015-04-29 23:24:49 -0500

    Se crea una interfaz llamada DAOFactura.

---
+++ codigo/EjerccioJPAclases/src/main/java/edu/co/sena/tennisshop/modelo/factory/DAOFactura.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.co.sena.tennisshop.modelo.factory;
+
+import edu.co.sena.tennishop.midelo.jpa.dao.interfaces.FacturaDAO;
+
+/**
+ *
+ * @author pc
+ */
+public interface DAOFactura {
+     public FacturaDAO createFacturaDAo();
+    
+}
