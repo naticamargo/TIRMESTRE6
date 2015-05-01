47e4b15645b7d25eea981e49042d5328c31388f5
naticamargo <natusca_camargo@hotmail.com>
2015-05-01 11:59:06 -0500

    se arreglo la interfaz producto

--- codigo/EjerccioJPAclases/src/main/java/edu/co/sena/tennishop/midelo/jpa/dao/interfaces/ProductoDAO.java
+++ codigo/EjerccioJPAclases/src/main/java/edu/co/sena/tennishop/midelo/jpa/dao/interfaces/ProductoDAO.java
@@ -5,7 +5,7 @@
  */
 package edu.co.sena.tennishop.midelo.jpa.dao.interfaces;
 
-import edu.co.sena.tennisshop.integracion.jpa.entitis.Producto;
+import edu.co.sena.entity.jpa.Producto;
 import java.util.List;
 
 /**
