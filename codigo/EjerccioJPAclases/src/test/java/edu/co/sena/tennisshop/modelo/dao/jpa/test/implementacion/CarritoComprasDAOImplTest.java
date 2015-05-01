a9068c3cbabfd4acb9139a2f34f8be88b2f2c30b
naticamargo <natusca_camargo@hotmail.com>
2015-05-01 16:23:04 -0500

    se termino carritocomprastest

--- codigo/EjerccioJPAclases/src/test/java/edu/co/sena/tennisshop/modelo/dao/jpa/test/implementacion/CarritoComprasDAOImplTest.java
+++ codigo/EjerccioJPAclases/src/test/java/edu/co/sena/tennisshop/modelo/dao/jpa/test/implementacion/CarritoComprasDAOImplTest.java
@@ -5,8 +5,9 @@
  */
 package edu.co.sena.tennisshop.modelo.dao.jpa.test.implementacion;
 
-import edu.co.sena.tennisshop.integracion.jpa.entitis.CarritoCompras;
+import edu.co.sena.entity.jpa.CarritoCompras;
 import edu.co.sena.tennisshop.modelo.jpa.dao.implementacion.CarritoComprasDAOImpl;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,7 +19,7 @@ import static org.junit.Assert.*;
  * @author ADMIN
  */
 public class CarritoComprasDAOImplTest {
-
+    CarritoCompras Entity = new CarritoCompras ();
     public CarritoComprasDAOImplTest() {
     }
 
@@ -28,6 +29,11 @@ public class CarritoComprasDAOImplTest {
 
     @Before
     public void setUp() {
+        Entity.setIdCARRITO("101");
+        Entity.setImpuestos(16);
+        Entity.setSubtotal(60000);
+        Entity.setTotal(61000);
+        
 
     }
 
@@ -43,38 +49,77 @@ public class CarritoComprasDAOImplTest {
     @Test
     public void testInsert() {
         System.out.println("insert");
-        CarritoCompras entity = new CarritoCompras();
-        entity.setIdCARRITO("asdfasdfasdf");
-        entity.setImpuestos(0);
-        entity.setSubtotal(0);
-        entity.setTotal(0);
         CarritoComprasDAOImpl instance = new CarritoComprasDAOImpl();
-        instance.insert(entity);
+        instance.insert(Entity);
 
     }
 
     @Test
     public void testUpdate() {
         System.out.println("update");
-        CarritoCompras entity = new CarritoCompras();
-        entity.setIdCARRITO("asdfasdfasdf");
-        entity.setImpuestos(1);
-        entity.setTotal(1000);
+        Entity.setImpuestos(1);
         CarritoComprasDAOImpl instance = new CarritoComprasDAOImpl();
-        instance.update(entity);
+        instance.update(Entity);
 
-//        fail("The test case is a prototype.");
+
     }
 
     @Test
     public void testDelete() {
-        System.out.println("delete");
-        CarritoCompras entity = new CarritoCompras();
-        entity.setIdCARRITO("asdfasdfasdf");
-       CarritoComprasDAOImpl instance = new CarritoComprasDAOImpl();
-        instance.delete(entity);
-//// TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
+         System.out.println("delete");
+        CarritoComprasDAOImpl instance = new CarritoComprasDAOImpl();
+        CarritoCompras ct = instance.findByIdCARRITO("101");
+        instance.delete(ct);
     }
+     @Test
+    public void testFindByIdCarrito() {
+        System.out.println("findByIdCARRITO");
+        CarritoCompras ct;
+        String idcarrito = "101";
+        CarritoComprasDAOImpl carrito = new CarritoComprasDAOImpl();
+        ct = carrito.findByIdCARRITO(idcarrito);
+        System.out.println(ct.toString());
 
+    }
+    @Test
+    public void testFindByAll() {
+        System.out.println("findByAll");
+        CarritoComprasDAOImpl instance = new CarritoComprasDAOImpl();
+        List<CarritoCompras> result = instance.findByAll();
+        for (CarritoCompras result1 : result) {
+            System.out.println(result1.toString());
+        }
+
+}
+     @Test
+    public void testFindByTotal() {
+        System.out.println("findByTotal");
+        double total = 61000;
+        CarritoComprasDAOImpl instance = new CarritoComprasDAOImpl();
+        List<CarritoCompras> result = instance.findByTotal(total);
+        for (CarritoCompras result1 : result) {
+            System.out.println(result1.toString());
+        }
+}
+     @Test
+    public void testFindBySubtotal() {
+        System.out.println("findBySubtotal");
+        double subtotal =60000 ;
+        CarritoComprasDAOImpl instance = new CarritoComprasDAOImpl();
+        List<CarritoCompras> result = instance.findBySubtotal(subtotal);
+         for (CarritoCompras result1 : result) {
+            System.out.println(result1.toString());
+        }
+    }
+     @Test
+    public void testFindByImpuestos() {
+        System.out.println("findByImpuestos");
+        double impuestos =16 ;
+        CarritoComprasDAOImpl instance = new CarritoComprasDAOImpl();
+        List<CarritoCompras> result = instance.findByImpuestos(impuestos);
+         for (CarritoCompras result1 : result) {
+            System.out.println(result1.toString());
+        }
+    }
+    
 }
