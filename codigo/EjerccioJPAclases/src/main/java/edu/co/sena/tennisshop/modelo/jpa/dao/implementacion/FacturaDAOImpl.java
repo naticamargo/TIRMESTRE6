4307e34568b08cfe54602dd2a59521ea14c4adee
naticamargo <natusca_camargo@hotmail.com>
2015-05-01 14:25:24 -0500

    se termino facturaDAO

--- codigo/EjerccioJPAclases/src/main/java/edu/co/sena/tennisshop/modelo/jpa/dao/implementacion/FacturaDAOImpl.java
+++ codigo/EjerccioJPAclases/src/main/java/edu/co/sena/tennisshop/modelo/jpa/dao/implementacion/FacturaDAOImpl.java
@@ -23,10 +23,10 @@ import javax.persistence.Query;
  */
 public class FacturaDAOImpl implements FacturaDAO {
 
-    public static final String ID_FACTURA = "Id_factura";
-    public static final String CUENTA = "cuenta";
+    
+    public static final String FECHA = "fecha";
     public static final String FECHAFACTURA = "fecha factura";
-    public static final String FORMAPAGO = "forma pago";
+    public static final String FORMAPAGO = "formapago";
 
     private EntityManager getEntityManager() {
         return EntityManagerHelper.getEntityManager();
@@ -98,94 +98,57 @@ public class FacturaDAOImpl implements FacturaDAO {
     }
 
     @Override
-    public Factura findByFactura() {
+    public Factura findByIdFactura(Integer IdFactura) {
         EntityManager em = getEntityManager();
-        List<Factura> FacturaTemp = null;
-        Query query = em.createNamedQuery("Factura.findAll");
+         Factura facturaTemporal = null;
         try {
-            FacturaTemp = query.getResultList();
+            facturaTemporal = em.find(Factura.class, IdFactura);
         } catch (RuntimeException re) {
-            System.out.println("error:----------------" + re.getMessage());
-        } finally {
-            EntityManagerHelper.closeEntityManager();
-        }
-        return (Factura) FacturaTemp;
-    }
-
-    public List<Factura> findByIdFactura(Integer IdFactura) {
-
-        EntityManager em = getEntityManager();
-        Factura FacturaTemp = null;
-        try {
-            FacturaTemp = em.find(Factura.class, IdFactura);
-        } catch (RuntimeException re) {
-            System.out.println("error:----------------" + re.getMessage());
+            System.out.println("erorrr:----------------" + re.getMessage());
         } finally {
             if (em != null) {
                 EntityManagerHelper.closeEntityManager();
             }
         }
-        return (List<Factura>) FacturaTemp;
-
+        return facturaTemporal;
     }
 
-    public List<Factura> findByCuenta(String Cuenta) {
-        EntityManager em = getEntityManager();
-        List<Factura> FacturaTemp = null;
-        Query query = em.createNamedQuery("Factura.");
-        try {
-            FacturaTemp = query.getResultList();
-        } catch (RuntimeException re) {
-            System.out.println("error:----------------" + re.getMessage());
-        } finally {
-            EntityManagerHelper.closeEntityManager();
-        }
-        return FacturaTemp;
-    }
-
+    @Override
     public List<Factura> findByFechaFactura(Date FechaFactura) {
-
         EntityManager em = getEntityManager();
-        List<Factura> FacturaTemp = null;
-        Query query = em.createNamedQuery("Factura.findByFechaFactura");
+        List<Factura> facTemporales = null;
         try {
-            FacturaTemp = query.getResultList();
+           Query query = em.createNamedQuery("Factura.findByFechaFactura");
+            query.setParameter(FacturaDAOImpl.FECHA, FechaFactura);
+            facTemporales = query.getResultList();
         } catch (RuntimeException re) {
-            System.out.println("error:----------------" + re.getMessage());
+            System.out.println("erorrr:----------------" + re.getMessage());
+        } finally {
+               EntityManagerHelper.closeEntityManager();
+            }
+          return  facTemporales;
+       
+    }
+
+    @Override
+    public List<Factura> findByFormaPago(String FormaPago) {
+        EntityManager em = getEntityManager();
+        List<Factura> facTemporal = null;
+
+        try {
+            Query query = em.createNamedQuery("Factura.findByFormaPago");
+            query.setParameter(FacturaDAOImpl.FORMAPAGO,FormaPago);
+            facTemporal = query.getResultList();
+        } catch (RuntimeException re) {
+            System.out.println("erorrr:----------------" + re.getMessage());
         } finally {
             EntityManagerHelper.closeEntityManager();
         }
-        return FacturaTemp;
+        return facTemporal;
     }
 
-    @Override
-    public List<Factura> findByFormaPago(Object FormaPago) {
-        EntityManager em = getEntityManager();
-        List<Factura> FacturaTemp = null;
-        Query query = em.createNamedQuery("Factura.findByFormaPago");
-        try {
-            FacturaTemp = query.getResultList();
-        } catch (RuntimeException re) {
-            System.out.println("error:----------------" + re.getMessage());
-        } finally {
-            EntityManagerHelper.closeEntityManager();
-        }
-        return FacturaTemp;
     }
 
-    @Override
-    public List<Factura> findByIdFactura(Object IdFactura) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    
 
-    @Override
-    public List<Factura> findByCuenta(Object Cuenta) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
-    @Override
-    public List<Factura> findByFechaFactura(Object FechaFactura) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-}
