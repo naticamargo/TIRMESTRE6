95032e8ea6d6f5de8c600b4a872fb6750f01bf73
familia <familia@familia-PC>
2015-05-01 13:24:02 -0500

    Ya esta la prueba

--- codigo/EjerccioJPAclases/src/test/java/edu/co/sena/tennisshop/modelo/dao/jpa/test/implementacion/UsuarioDAOImplTest.java
+++ codigo/EjerccioJPAclases/src/test/java/edu/co/sena/tennisshop/modelo/dao/jpa/test/implementacion/UsuarioDAOImplTest.java
@@ -7,6 +7,7 @@ package edu.co.sena.tennisshop.modelo.dao.jpa.test.implementacion;
 
 import edu.co.sena.entity.jpa.Usuario;
 import edu.co.sena.tennisshop.modelo.jpa.dao.implementacion.UsuarioDAOImpl;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -14,32 +15,34 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
 /**
  *
  * @author ColsutecR
  */
 public class UsuarioDAOImplTest {
+
     Usuario Entity = new Usuario();
+
     public UsuarioDAOImplTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
         Entity.setIdUsuario("1");
         Entity.setCorreoElectronico("cristiancho.96@hotmail.com");
         Entity.setContrasena("123456789");
         Entity.setActivo(Boolean.TRUE);
-        Entity.setRol("cliente");    }
-    
+        Entity.setRol("cliente");
+    }
+
     @After
     public void tearDown() {
     }
@@ -62,7 +65,8 @@ public class UsuarioDAOImplTest {
         instance.insert(entity);
 
     }
-@Test
+
+    @Test
     public void testUpdate() {
         System.out.println("update");
         Usuario entity = new Usuario();
@@ -75,6 +79,7 @@ public class UsuarioDAOImplTest {
         instance.update(entity);
 
     }
+
     @Test
     public void testDelete() {
         System.out.println("delete");
@@ -85,5 +90,56 @@ public class UsuarioDAOImplTest {
 //// TODO review the generated test code and remove the default call to fail.
 //        fail("The test case is a prototype.");
     }
+
+    @Test
+    public void testFindByAll() {
+        System.out.println("findByAll");
+        UsuarioDAOImpl instance = new UsuarioDAOImpl();
+        List<Usuario> result = instance.findByAll();
+        for (Usuario result1 : result) {
+            System.out.println(result1.getIdUsuario());
+        }
+    }
     
+    @Test
+    public void testFindByCorreoElectronico() {
+        System.out.println("findByCorreoElectronico");
+        String CorreoElectronico = "cristiancho.96@hotmail.com";
+        UsuarioDAOImpl instance = new UsuarioDAOImpl();
+        List<Usuario> result = instance.findByCorreoElectronico(CorreoElectronico);
+        for (Usuario result1 : result) {
+            System.out.println(result1.getIdUsuario());
+        }
+    }
+    
+    @Test
+    public void testFindByContrasena() {
+        System.out.println("findByContrasena");
+        String Contrasena = "123456789";
+        UsuarioDAOImpl instance = new UsuarioDAOImpl();
+        List<Usuario> result = instance.findByContrasena(Contrasena);
+        for (Usuario result1 : result) {
+            System.out.println(result1.getIdUsuario());
+        }
+    }
+    @Test
+    public void testFindByRol() {
+        System.out.println("findByRol");
+        String Rol = "Cliente";
+        UsuarioDAOImpl instance = new UsuarioDAOImpl();
+        List<Usuario> result = instance.findByRol(Rol);
+        for (Usuario result1 : result) {
+            System.out.println(result1.getIdUsuario());
+        }
+    }
+    @Test
+    public void testFindByActivo() {
+        System.out.println("findByActivo");
+        String activo = "";
+        UsuarioDAOImpl instance = new UsuarioDAOImpl();
+        List<Usuario> result = instance.findByActivo(activo);
+        for (Usuario result1 : result) {
+            System.out.println(result1.getIdUsuario());
+        }
+    }
 }
