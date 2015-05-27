adda7702c9128ec5c2ae48d3d2046766d6c62d90
naticamargo <natusca_camargo@hotmail.com>
2015-05-26 20:56:20 -0500

    se hizo arreglo LOS PAQUETES

--- codigo/EjerccioJPAclases/src/test/java/DepartamentoJPATest.java
+++ codigo/EjerccioJPAclases/src/test/java/DepartamentoJPATest.java
@@ -4,9 +4,9 @@
  * and open the template in the editor.
  */
 import edu.co.sena.jpa.util.EntityManagerHelper;
-import edu.co.sena.tennisshop.integracion.jpa.entitis.Cuenta;
-import edu.co.sena.tennisshop.integracion.jpa.entitis.Departamento;
-import edu.co.sena.tennisshop.integracion.jpa.entitis.Inventario;
+import edu.co.sena.entity.jpa.Cuenta;
+import edu.co.sena.entity.jpa.Departamento;
+import edu.co.sena.entity.jpa.Inventario;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
